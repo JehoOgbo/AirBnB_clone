3c1387348bc3e89b26ee31a2af92895a6399ad35
Urom Jehoshaphat <uromjehoshaphat@gmail.com>
2025-05-28 16:55:45 +0100

    fix a problem

--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,36 @@
 #!/usr/bin/python3
-""" This is a class which defines all common attributes/methods for
-    other classes
-"""
-from datetime import datetime
+""" defines all common attributes/methods for other classes """
 import uuid
+import datetime
 
 
-class BaseModel():
-
-    """ This serves as a base model for other classes in this package """
-    def __init__(self, *args, **kwargs):
-        """ initialize instance attributes"""
-        if kwargs and len(kwargs) != 0:
-            if '__class__' in kwargs:
-                del kwargs['__class__']
-            kwargs['created_at'] = datetime.fromisoformat(kwargs['created_at'])
-            kwargs['updated_at'] = datetime.fromisoformat(kwargs['updated_at'])
-            self.__dict__.update(kwargs)
-        else:
-            self.id = str(uuid.uuid4())  # creates unique id for each basemodel
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            from .__init__ import storage
-            storage.new(self)
+class BaseModel:
+    """ define the attributes of the base model """
+    def __init__(self):
+        """ initialize the basemodel when called """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        """ print a string representation of the object """
-        classy = type(self)
-        return ("[{}] ({}) {}".format(classy.__name__, self.id, self.__dict__))
+        """ returns  [<class name>] (<self.id>) <self.__dict__> """
+        return f"[{(type(self)).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """ Updates updated_at with the current datetime """
-        self.updated_at = datetime.now()
-        from .__init__ import storage
-        storage.save()
+        """ update the instance with current datetime """
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """ returns a dict containting all keys/values of the instance """
-        the_dict = dict(self.__dict__)
-        the_dict.update({'__class__': type(self).__name__,
-                         'updated_at': self.updated_at.isoformat(),
-                         'id': self.id,
-                         'created_at': self.created_at.isoformat()})
-        return the_dict
+        """
+        returns a dictionary containing all
+        keys/values of __dict__ of the instance
+        """
+        new = self.__dict__
+        newer = {}
+        for key, value in new.items():
+            if key == 'created_at' or key == 'updated_at':
+                newer[key] = value.isoformat()
+                continue
+            newer[key] = value
+        newer['__class__'] = __class__.__name__
+        return newer
